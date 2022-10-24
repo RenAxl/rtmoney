3b2c9497a375ee8d29695d56cff7e5410b543b80
Renan Duarte <renandg.axl@gmail.com>
2022-10-24 10:58:39 -0300

    Project created

---
+++ backend/src/main/java/com/thayren/rtmoney/RtmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.rtmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtmoneyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thayren/rtmoney/RtmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.rtmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
