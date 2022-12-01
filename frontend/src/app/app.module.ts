b33e286fff57d2b5797e0403876080a058498000
Renan Duarte <renandg.axl@gmail.com>
2022-12-01 10:19:23 -0300

    Basic route setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,15 @@ import { AppComponent } from './app.component';
 import { ReleasesModule } from './releases/releases.module';
 import { PersonsModule } from './persons/persons.module';
 import { CoreModule } from './core/core.module';
+import { ResearchReleasesComponent } from './releases/research-releases/research-releases.component';
+import { ReleaseRegisterComponent } from './releases/release-register/release-register.component';
+import { ResearchPersonsComponent } from './persons/research-persons/research-persons.component';
 
+const routes: Routes = [
+  { path: 'releases', component: ResearchReleasesComponent },
+  { path: 'releases/new', component: ReleaseRegisterComponent },
+  { path: 'persons', component: ResearchPersonsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +28,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule, 
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
 
     AppRoutingModule, 
     ReleasesModule, 
