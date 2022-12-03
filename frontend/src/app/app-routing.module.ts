743a4d6b6184137288bc4610400fb9708650cc52
Renan Duarte <renandg.axl@gmail.com>
2022-12-03 10:50:33 -0300

    Refactoring routes to use Routing Module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found.component';
+import { ResearchPersonsComponent } from './persons/research-persons/research-persons.component';
+import { ReleaseRegisterComponent } from './releases/release-register/release-register.component';
+import { ResearchReleasesComponent } from './releases/research-releases/research-releases.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'releases', pathMatch: 'full' },
+  { path: 'releases', component: ResearchReleasesComponent },
+  { path: 'releases/new', component: ReleaseRegisterComponent },
+  { path: 'releases/:id', component: ReleaseRegisterComponent },
+  { path: 'persons', component: ResearchPersonsComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'page-not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,20 +9,6 @@ import { AppComponent } from './app.component';
 import { ReleasesModule } from './releases/releases.module';
 import { PersonsModule } from './persons/persons.module';
 import { CoreModule } from './core/core.module';
-import { ResearchReleasesComponent } from './releases/research-releases/research-releases.component';
-import { ReleaseRegisterComponent } from './releases/release-register/release-register.component';
-import { ResearchPersonsComponent } from './persons/research-persons/research-persons.component';
-import { PageNotFoundComponent } from './core/page-not-found.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'releases', pathMatch: 'full' },
-  { path: 'releases', component: ResearchReleasesComponent },
-  { path: 'releases/new', component: ReleaseRegisterComponent },
-  { path: 'releases/:id', component: ReleaseRegisterComponent },
-  { path: 'persons', component: ResearchPersonsComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: 'page-not-found' }
-];
 
 @NgModule({
   declarations: [
@@ -33,8 +18,7 @@ const routes: Routes = [
     BrowserModule, 
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
-
+  
     AppRoutingModule, 
     ReleasesModule, 
     PersonsModule,
