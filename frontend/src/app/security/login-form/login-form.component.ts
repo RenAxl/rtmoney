1f8d37b9db9f67afc60427b03598f99333b910cb
Renan Duarte <renandg.axl@gmail.com>
2022-12-06 15:31:45 -0300

    Handling authentication errors, redirecting pages and showing success and authentication error messages.

--- frontend/src/app/security/login-form/login-form.component.ts
+++ frontend/src/app/security/login-form/login-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,13 +12,25 @@ import { AuthService } from '../auth.service';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router,
+    private messageService: MessageService,
+    ) { }
 
   ngOnInit(): void {
   }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
+    this.auth.login(usuario, senha)
+    .then(() => {
+      this.router.navigate(['/releases']);
+      this.messageService.add({ severity: 'success', detail: 'Usuário autenticado com sucesso!' });
+    })
+    .catch(erro => {
+      this.errorHandler.handle(erro);
+    });
   }
 
 }
