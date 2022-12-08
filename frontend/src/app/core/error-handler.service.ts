69ff376ec8148bf5efa11000374f90ce4a6d739c
Renan Duarte <renandg.axl@gmail.com>
2022-12-08 15:23:19 -0300

    When the Refresh Token expires, send a message to the user informing that the session has expired and redirect him to the login page.

--- frontend/src/app/core/error-handler.service.ts
+++ frontend/src/app/core/error-handler.service.ts
@@ -1,19 +1,30 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { NotAuthenticatedError } from '../security/money-http-interceptor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private router: Router
+    ) { }
 
   handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      console.log('erro refresh');
+
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+       
     } else if (errorResponse instanceof HttpErrorResponse
       && errorResponse.status >= 400 && errorResponse.status <= 499) {
       msg = 'Ocorreu um erro ao processar a sua solicitação';
@@ -26,8 +37,8 @@ export class ErrorHandlerService {
         } catch (e) { }
       }   
       
+        console.error('Ocorreu um erro', errorResponse);
 
-      console.error('Ocorreu um erro', errorResponse);
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
